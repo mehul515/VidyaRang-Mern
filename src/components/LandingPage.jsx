04f3034faf48a92b1eccc2957747a026c022edad
mehul515 <mehulkohad@gmail.com>
2025-02-24 19:41:32 +0530

    update folder structure

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Footer from './Footer'
+
+export default function LandingPage() {
+  return (
+    <div>
+    Add other components here, like hero section, features, etc
+      Landing Page
+      <Footer/>
+    </div>
+  )
+}
