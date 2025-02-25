5739a27eaaa330973365e54e45c6fc13cc8bfea8
mehul515 <mehulkohad@gmail.com>
2025-02-25 22:35:17 +0530

    add chat interface component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,15 @@
+"use client"
+
 import React from 'react'
 import { motion } from "framer-motion";
-import  LampContainer  from "../components/ui/lamp"
+import LampContainer from "../components/ui/lamp"
 export default function About() {
   return (
     <div className='text-white p-24'>
       <div className='w-full  h-96 bg-white rounded-2xl'>
-      <img className='w-full h-full object-cover rounded-2xl' src="/assets/main.png" alt="" />
+        <img className='w-full h-full object-cover rounded-2xl' src="/assets/main.png" alt="" />
       </div>
-   
+
     </div>
   )
 }
