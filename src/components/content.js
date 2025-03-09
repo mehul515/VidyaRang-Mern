8daeafe7e60d6878fc91478db55aa56b860d6c20
Chitraksh09error <147639202+Chitraksh09error@users.noreply.github.com>
2025-03-09 20:39:47 +0530

    about updates

---
+++ src/components/content.js
@@ -0,0 +1,7 @@
+export const array=[
+    {id:1 , des : '1. Select "Create New Course" '},
+    {id:2 , des : '2. Enter course name'},
+    {id:3 , des : '3. Upload your files'},
+    {id:4 , des : '4. After processing your files course will be on-boarded'},
+    {id:5 , des : 'Select "Course chat" option to chat with your course material '}
+];
