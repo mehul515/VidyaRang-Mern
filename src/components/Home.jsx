79063096e96487b058aa2d4a1800c764e6b1dbf3
Chitraksh09error <147639202+Chitraksh09error@users.noreply.github.com>
2025-03-23 21:48:00 +0530

    audio updatesss

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import {React, useState, useRef} from 'react'
 import Createcourse from './Createcourse'
 import Videobox from './Videobox'
-
+import { FaPlay } from "react-icons/fa";
+import { FaPause } from "react-icons/fa";
 export default function Home() {
+
+   const [isPlaying, setIsPlaying] = useState(false);
+    const audioRef = useRef(null);
+  
+    const togglePlayPause = () => {
+      if (isPlaying) {
+        audioRef.current.pause();
+      } else {
+        audioRef.current.currentTime = 0;
+        audioRef.current.play();
+      }
+      setIsPlaying(!isPlaying);
+    };
+  
+    const handleAudioEnd = () => {
+      setIsPlaying(false); // Reset button to "Play" when audio finishes
+    };
+  
   return (
     <div className=" mt-20 h-screen lg:pb-28 pb-72 overflow-auto lg:px-6 px-2 text-center flex flex-col ">
 
@@ -15,6 +34,13 @@ export default function Home() {
       <div className=" p-2 mt-5 lg:text-2xl text-xl cursor-not-allowed ">
         <h1 className=" text-gray-300">An AIGurukul Initiative - Build by students for students</h1>
       </div>
+       <div className="mt-6">
+      
+              <audio ref={audioRef}  onEnded={handleAudioEnd} src="/assets/vidyarang.wav" preload="auto" />
+              <button className=" text-lg border text-cyan-400 bg-black border-gray-700 p-2.5 px-6 rounded-xl" onClick={togglePlayPause}>
+              {isPlaying ? <FaPause /> :  <FaPlay />}
+            </button>
+            </div>
 
       <Createcourse />
 
